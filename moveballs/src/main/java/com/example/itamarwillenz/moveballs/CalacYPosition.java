f0914e1a4aecd6876f76ac632ba765a26dfea828
itamar.willenz <itamar.willenz@itamar.willenz-mbp>
2016-01-20 10:53:56 +0200

    first commit

---
+++ moveballs/src/main/java/com/example/itamarwillenz/moveballs/CalacYPosition.java
@@ -0,0 +1,7 @@
+package com.example.itamarwillenz.moveballs;
+
+/**
+ * Created by itamar.willenz on 1/19/16.
+ */
+public class CalacYPosition {
+}
---
+++ moveballs/src/main/java/com/example/itamarwillenz/moveballs/onYPositionReCalaListener.java
@@ -0,0 +1,9 @@
+package com.example.itamarwillenz.moveballs;
+
+/**
+ * Created by itamar.willenz on 1/19/16.
+ */
+public interface onYPositionReCalaListener {
+    void moveUp();
+    void moveDown();
+}
