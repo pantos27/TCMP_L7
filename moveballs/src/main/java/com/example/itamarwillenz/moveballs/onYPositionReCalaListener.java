90f67a9d01f2ff0748cb5eb5e9f465076d8f9de1
itamar.willenz <6587willenz>
2016-02-13 21:28:21 +0200

    moving balls

--- moveballs/src/main/java/com/example/itamarwillenz/moveballs/onYPositionReCalaListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.itamarwillenz.moveballs;
-
-/**
- * Created by itamar.willenz on 1/19/16.
- */
-public interface onYPositionReCalaListener {
-    void moveUp();
-    void moveDown();
-}
