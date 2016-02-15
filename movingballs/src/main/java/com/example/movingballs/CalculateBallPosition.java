c31b03d3b05788d4f09119e260ba96843f398ebf
itamar.willenz <itamar.wz@gmail.com>
2016-02-15 14:30:19 +0200

    Moving Balls #3

--- movingballs/src/main/java/com/example/movingballs/CalculateBallPosition.java
+++ movingballs/src/main/java/com/example/movingballs/CalculateBallPosition.java
@@ -6,9 +6,13 @@ import android.view.View;
 import android.os.Handler;
 import android.widget.Toast;
 
+import java.util.Random;
+
 /**
  * Created by itamar.willenz on 13/02/2016.
  */
+
+
 public class CalculateBallPosition extends Thread {
 
     View blue_ball;
@@ -17,34 +21,83 @@ public class CalculateBallPosition extends Thread {
     int maxX;
     Handler handler;
 
-    int blue_ball_new_x;
-    int blue_ball_new_y;
-    int red_ball_new_x;
-    int red_ball_new_y;
+    int directionX_blue_ball = 1;
+    int directionY_blue_ball = 1;
+
+    int directionX_red_ball = 1;
+    int directionY_red_ball = 1;
+
+    int velocity = 5;
 
     public CalculateBallPosition(View blue_ball, View red_ball, Handler handler, int maxX, int maxY) {
         this.blue_ball = blue_ball;
         this.red_ball = red_ball;
         this.handler = handler;
-        this.maxX = 100;
+        this.maxX = maxX;
         this.maxY = maxY;
+
+        blue_ball_new_x = random.nextInt(maxX - blue_ball.getWidth());
+        blue_ball_new_y = random.nextInt(maxY - blue_ball.getWidth());
+        red_ball_new_x = random.nextInt(maxX - blue_ball.getWidth());
+        red_ball_new_y = random.nextInt(maxY - blue_ball.getWidth());
+
     }
 
     @Override
     public void run() {
+        moveBalls(blue_ball, red_ball);
+    }
 
-        for (int i = 0; i < maxX ; i++) {
+    Random random = new Random();
 
-            blue_ball_new_x++;
-            red_ball_new_x++;
+    int blue_ball_new_x;
+    int blue_ball_new_y;
+    int red_ball_new_x;
+    int red_ball_new_y;
+
+
+    public void moveBalls(View blue_ball, View red_ball) {
+
+        while (true) {
+
+            // blue ball condition
+            if (blue_ball_new_x > (maxX - blue_ball.getWidth())
+                    || blue_ball_new_x < 0) {
+                directionX_blue_ball *= -1;
+            }
+
+            if (blue_ball_new_y > (maxY - blue_ball.getHeight())
+                    || blue_ball_new_y < 0) {
+                directionY_blue_ball *= -1;
+            }
+
+            // red ball condition
+            if (red_ball_new_x > (maxX - red_ball.getWidth())
+                    || red_ball_new_x < 0) {
+                directionX_red_ball *= -1;
+            }
+
+            if (red_ball_new_y > (maxY - red_ball.getHeight())
+                    || red_ball_new_y < 0) {
+                directionY_red_ball *= -1;
+            }
+
+            // change the cordinations of the balls
+            blue_ball_new_x += directionX_blue_ball;
+            blue_ball_new_y += directionY_blue_ball;
+
+            red_ball_new_x += directionX_red_ball;
+            red_ball_new_y += directionY_red_ball;
 
             handler.post(setBalls);
-
-
-
+            try {
+                Thread.sleep(velocity);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        
     }
+
 
     Runnable setBalls = new Runnable() {
         @Override
@@ -53,12 +106,11 @@ public class CalculateBallPosition extends Thread {
             blue_ball.setY(blue_ball_new_y);
             red_ball.setX(red_ball_new_x);
             red_ball.setY(red_ball_new_y);
-            red_ball.invalidate();
-            blue_ball.invalidate();
-
-            Log.d("Hi", "HI");
         }
     };
 
-    // Todo int maxX, and int maxY are returning 0, need to see why.
+
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
 }
--- movingballs/src/main/java/com/example/movingballs/MainActivity.java
+++ movingballs/src/main/java/com/example/movingballs/MainActivity.java
@@ -6,20 +6,21 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 
 import android.os.Handler;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     View blue_ball;
     View red_ball;
     int max_hight;
     int max_width;
 
-
+    LinearLayout ballView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +30,52 @@ public class MainActivity extends Activity {
         blue_ball = findViewById(R.id.blue_ball);
         red_ball = findViewById(R.id.red_ball);
 
-        max_hight = ((LinearLayout) blue_ball.getParent()).getHeight();
-        max_width = ((LinearLayout) blue_ball.getParent()).getWidth();
 
-        new CalculateBallPosition(blue_ball, red_ball, new Handler() {
+
+        // Here we get the max width and hight of the Linear Layout
+        ballView = (LinearLayout) findViewById(R.id.ball_view);
+
+        ballView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
-            public void handleMessage(Message msg) {
-                Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_SHORT).show();
-            }
-        }, max_width, max_hight).start();
+            public void onGlobalLayout() {
 
+                max_hight = ballView.getHeight();
+                max_width = ballView.getWidth();
+
+//                 this is where i should implement the random positioning of the balls
+//                blue_ball.setX(180);
+//                blue_ball.setY(180);
+
+                ballView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                new CalculateBallPosition(blue_ball, red_ball, new Handler() {
+                    @Override
+                    public void handleMessage(Message msg) {
+                        Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_SHORT).show();
+                    }
+                }, max_width, max_hight).start();
+            }
+        });
+
+
+
+        // This happens before i get the Max width and Max hight
+
+//        new CalculateBallPosition(blue_ball, red_ball, new Handler() {
+//            @Override
+//            public void handleMessage(Message msg) {
+//                Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_SHORT).show();
+//            }
+//        }, max_width, max_hight).start();
+//
     }
 
+
+    // implement the speed change of the balls
+    @Override
+    public void onClick(View v) {
+
+
+
+    }
 }
